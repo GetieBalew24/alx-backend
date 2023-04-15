00171459e64e174310a7658d6ae36e66f8726dc5
GetieBalew24 <getiebalew2020@gmail.com>
2023-04-15 14:11:20 -0400

    simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """
-Adds `get_page` method to `Server` class
+Copy index_range from the previous task to `Server` class
 """
 import csv
-from typing import List, Tuple
-
+import math
+from typing import List
 
 class Server:
     """Server class to paginate a database of popular baby names.
@@ -45,16 +45,16 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
-        Get items for the given page number
+        return items for the given page number range
         Args:
-            page (int): page number
-            page_size (int): number of items per page
+            page (int): the current page number
+            page_size (int): number of rows per page
         Returns:
-            (List[List]): a list of list(row) if inputs are within range
-            ([]) : an empty list if page and page_size are out of range
+            (List[List]): a list of row if page and page size > 0
+            ([]) : an empty list if page and page_size < 0
         """
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
-        startIndex, endIndex = self.index_range(page, page_size)
-        return self.dataset()[startIndex:endIndex]
+        FirstIndex, LastIndex = self.index_range(page, page_size)
+        return self.dataset()[FirstIndex:LastIndex]
 
