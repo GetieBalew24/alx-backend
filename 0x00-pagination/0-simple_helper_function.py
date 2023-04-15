c37b7aa5d617df4eca5bd578bd4a02bda97c96f2
GetieBalew24 <getiebalew2020@gmail.com>
2023-04-15 13:44:09 -0400

    index range

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -6,13 +6,14 @@ def index_range(page: int, page_size: int) -> tuple:
     """
     Args:
         page (int): the current page
-        page_size (int): the amount of items in a page
+        page_size (int): the size of items in a page
         first_index: calculate the first index
-        last_index: calculate the last index  
+        last_index: calculate the last index 
+        indexOfrange: return first index and last index 
     Returns:
-        (tuple): a tuple of the start and end index for the given page
+        (tuple): a row/tuple of the start and end index of the given page
     """
     first_index = (page - 1) * page_size
     last_index = first_index + page_size
-    #indexOfrange = first_index, last_index
-    return first_index, last_index
+    indexOfrange = first_index, last_index
+    return indexOfrange
