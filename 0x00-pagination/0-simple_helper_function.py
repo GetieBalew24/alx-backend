6ef05129bb0ece0c730d14d148176a2b990526aa
GetieBalew24 <getiebalew2020@gmail.com>
2023-04-15 13:19:41 -0400

    try to fix it

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 """
 Module 0-simple_helper_function
 """
@@ -7,11 +6,7 @@ Module 0-simple_helper_function
 
 def index_range(page: int, page_size: int) -> tuple:
     """
-    Takes two integer arguments page and page_size.
-    Returns a tuple of size two containing a start
-    index and an end index corresponding to the range
-    of indexes to return in a list for those particular
-    pagination parameters.
+    Takes two integer arguments page
     """
     index_tuple = page_size * (page - 1), page * page_size
     return index_tuple
