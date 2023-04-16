9d74678e2901221858106ae291a4e0436e3999fd
GetieBalew24 <getiebalew2020@gmail.com>
2023-04-16 13:06:24 -0400

    hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -54,3 +54,18 @@ class Server:
         assert page > 0 and page_size > 0
         FirstIndex, LastIndex = self.index_range(page, page_size)
         return self.dataset()[FirstIndex:LastIndex]
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """hypermedia pagination """
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
+        return {
+                "page_size": len(data),
+                "page": page,
+                "data": data,
+                "next_page": next_page,
+                "prev_page": prev_page,
+                "total_pages": total_pages
+                }
