eaed686ef9cfc425fd054d74ee8432b2ace04d86
GetieBalew24 <getiebalew2020@gmail.com>
2023-04-16 13:18:15 -0400

    hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,71 +1,75 @@
+
+ 
+
 #!/usr/bin/env python3
-"""index_range from the previous task to `Server` class """
+
+"""
+Module 2-hypermedia_pagination
+"""
 import csv
 import math
 from typing import List
 
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Takes two integer arguments page and page_size.
+    Returns a tuple of size two containing a start
+    index and an end index corresponding to the range
+    of indexes to return in a list for those particular
+    pagination parameters.
+    """
+    index_tuple = page_size * (page - 1), page * page_size
+    return index_tuple
+
+
 class Server:
-    """Server class to paginate a database of popular baby names. """
-    Data = "Popular_Baby_Names.csv"
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """init object"""
         self.__dataset = None
 
     def dataset(self) -> List[List]:
         """Cached dataset
         """
         if self.__dataset is None:
-            with open(self.Data) as f:
+            with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
             self.__dataset = dataset[1:]
-
         return self.__dataset
-
-    @staticmethod
-    def index_range(page: int, page_size: int) -> tuple:
-        """ 
-        Args:
-        page (int): the current page
-        page_size (int): the size of items in a page
-        first_index: calculate the first index
-        last_index: calculate the last index 
-        indexOfrange: return first index and last index 
-        Returns:
-        (tuple): a row/tuple of the start and end index of the given page
-        """
-    
-        first_index = (page - 1) * page_size
-        last_index = first_index + page_size
-        indexOfrange = first_index, last_index
-        return indexOfrange
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
-        return items for the given page number range
-        Args:
-            page (int): the current page number
-            page_size (int): number of rows per page
-        Returns:
-            (List[List]): a list of row if page and page size > 0
-            ([]) : an empty list if page and page_size < 0
+        paginate dataset
         """
-        assert type(page) == int and type(page_size) == int
-        assert page > 0 and page_size > 0
-        FirstIndex, LastIndex = self.index_range(page, page_size)
-        return self.dataset()[FirstIndex:LastIndex]
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+
+        indexes = index_range(page, page_size)
+        try:
+            data = self.dataset()
+            return data[indexes[0]: indexes[1]]
+        except IndexError:
+            return []
+
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
-        """hypermedia pagination"""
-        data_set = self.get_page(page, page_size)
-        total_page = math.ceil(len(self.dataset()) / page_size)
-        next_pages = page + 1 if page < total_page else None
-        prev_pages = page - 1 if page > 1 else None
+        """
+        Illustration of hypermedia pagination
+        """
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
 
         return {
-                "page_size": len(data_set),
+                "page_size": len(data),
                 "page": page,
-                "data": data_set,
-                "next_page": next_pages,
-                "prev_page": prev_pages,
-                "total_pages": total_page
+                "data": data,
+                "next_page": next_page,
+                "prev_page": prev_page,
+                "total_pages": total_pages
                 }
