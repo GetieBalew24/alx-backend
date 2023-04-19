82ceb8fd099ee0fa5a6608f482e2ca1f6fd0c0d7
GetieBalew24 <getiebalew2020@gmail.com>
2023-04-19 07:38:03 -0400

    basic cacheing

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -8,11 +8,17 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ Overide methods defined in basecaching"""
+    """ Overide methods defined in basecaching
+    Args:
+        key: the dictionary key.
+        item: the data items
+    Returns:
+        result: return the value of data items.
+    """
 
     def put(self, key, item):
         """assign to the dictionary self.cache_data 
-        the item value for the key key"""
+        the item value by key"""
         if key and item:
             self.cache_data[key] = item
     def get(self, key):
